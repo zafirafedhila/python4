e065054b06e9f759910d64f880d51dcc08e28c36
z <zafirafedhila@users.noreply.github.com>
2019-08-25 18:41:22 +0700

    Add files via upload

---
+++ input_standard.py
@@ -0,0 +1,29 @@
+# nama = input('Masukkan Nama: ')
+# tahun = int(input('... tahun anda lahir? '))
+# print(f'Kamu adalah {nama} umur kamu {2018-tahun}')
+
+
+#a
+Fakultas = input ('F1:')
+JumlahMahasiwaF1= int(int('36'))
+print(f'jumlah {Fakultas} jumlah peserta dalam satu team{36/3}')
+
+Fakultas = input ('F2:')
+JumlahMahasiwaF2= int(int('60'))
+print(f'jumlah {Fakultas} jumlah peserta dalam satu team{60/5}')
+
+Fakultas = input ('F3:')
+JumlahMahasiwaF2= int(int('72'))
+print(f'jumlah {Fakultas} jumlah peserta dalam satu team{72/6}')
+
+Fakultas = input ('F4:')
+JumlahMahasiwaF2= int(int('60'))
+print(f'jumlah {Fakultas} jumlah peserta dalam satu team{60/5}')
+
+Fakultas = input ('F5:')
+JumlahMahasiwaF2= int(int('12'))
+print(f'jumlah {Fakultas} jumlah peserta dalam satu team{12/1}')
+
+
+
+
